f9c96626e877ce399ff3634c34bc464cf9e5f9f7
roopkumard19 <roopkumard19@github.com>
2017-06-13 15:53:40 -0700

    testing

--- app/app.sh
+++ app/app.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
+#Initial wifi interface configuration
+ifconfig $1 up 10.0.0.1 netmask 255.255.255.0
+sleep 2
+ 
+###########Start dnsmasq, modify if required##########
+if [ -z "$(ps -e | grep dnsmasq)" ]
+then
+ dnsmasq
+fi
+###########
+ 
+#Enable NAT
+iptables --flush
+iptables --table nat --flush
+iptables --delete-chain
+iptables --table nat --delete-chain
+iptables --table nat --append POSTROUTING --out-interface $2 -j MASQUERADE
+iptables --append FORWARD --in-interface $1 -j ACCEPT
+ 
+sysctl -w net.ipv4.ip_forward=1
+ 
+#start hostapd
+hostapd /etc/hostapd/hostapd.conf 1> /dev/null
+killall dnsmasq
 
-echo hello world
