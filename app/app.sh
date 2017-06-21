2bf7bfe9789ed63640c0d300002d061ebd383dc9
roopkumard19 <roopkumard19@github.com>
2017-06-20 18:26:04 -0700

    testing

--- app/app.sh
+++ app/app.sh
@@ -2,5 +2,3 @@
 
 # Disable Network Manager wireless
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
-dbus-send --system --print-reply --dest=org.freedesktop.NetworkManager /org/freedesktop/NetworkManager org.freedesktop.DBus.Properties.Set string:"org.freedesktop.NetworkManager" string:"WirelessEnabled" variant:boolean:false
-
