2c6383a955809a2a244c2f7ba41b77e44afcabcf
roopkumard19 <roopkumard19@github.com>
2017-06-21 00:14:16 -0700

    update

--- app/app.sh
+++ app/app.sh
@@ -4,5 +4,13 @@ export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 
 sleep 5
 
+ifconfig wlan1 down
+
+sleep 2
+
+ifconfig wlan1 up
+
+sleep 5
+
 python ./activate_connection.py resin-hotspot
 
